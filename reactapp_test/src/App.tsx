7bd0bfd2217929ec8b9b72abc173b9f0bfde8293
akshaymurari <akshaymurari184@gmail.com>
2021-06-20 21:14:04 +0530

    added charts.js

--- reactapp_test/src/App.tsx
+++ reactapp_test/src/App.tsx
@@ -6,6 +6,7 @@ import Auth from "./Components/Auth/Auth";
 import Main from "./Components/Main/Main";
 import {Provider} from "react-redux";
 import {store} from "./Components/redux/store";
+import Charts from "./Components/Charts/Charts";
 
 export const graphql = "http://localhost:8000/graphql";
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/main" exact component={Main}></Route>
           <Route path="/events" exact component={()=><>in main</>}></Route>
           <Route path="/bookings" exact component={()=><>in main</>}></Route>
+          <Route path="/charts" exact component={Charts}></Route>
           <Route path="/error" exact component={()=><h1>in error page</h1>}></Route>
         </Switch>
       </Provider>
